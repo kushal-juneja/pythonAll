182377f3dfe7a7da0169828aa3eea5887d32d066
kushal.juneja <kushal.juneja91@gmail.com>
2017-02-13 21:58:16 -0600

    Chat Server

---
+++ Basic Chat Server Using Socket/cybraryNetworkPy2.py
@@ -0,0 +1,18 @@
+'''
+Objective
+1. Create a simple client app
+2. Connect to the server port
+3. Infinite loop and accept user input
+'''
+
+import socket
+
+#Create a TCP/IP socket. No need to bind it, as it a client side app. Can use any random port
+sock= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+#Connect to server
+sock.connect(('127.0.0.1',50001))
+
+#Infinite loop to Accept user input and send to server
+while(True):
+    sock.send(input().encode())
---
+++ Basic Chat Server Using Socket/cybraryNetwrokPy1.py
@@ -0,0 +1,40 @@
+'''
+Objective:
+Create a simple chat server application
+1. Create a socket object for IP and TCP and bind to port
+2. Accept one connection
+3. Set in receive mode
+'''
+
+import socket
+
+#Create a socket object
+#socket.AF_INET is for IPV4 socket
+#SOCK_STREAM is for TCP
+#SOCK_RAW is for RAW socket
+sock= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+
+#Host has double qoutes= To bind the socket to all the interfaces/Ip addresses of the machine
+#It can be IP address/Interface too
+HOST=''
+PORT=50001
+
+
+#Bind takes a tuple
+sock.bind((HOST,PORT ))
+
+
+#Listen to socket. Waiting for a client to attempt a connection
+#1 connection
+sock.listen(1)
+
+
+#Accept the connection and create a new socket for tat. Store tat in conn object. Address of client in addr
+conn,addr=sock.accept()
+
+
+#Data receive loop
+while(True):
+    conn.recv(1024)
+
